5fc1e9663d2fa47758715feae305aa58f233e9ae
Kate Aksay <gozdeaksay@gmail.com>
2024-09-05 15:55:57 -0600

    backend wip

--- api/build/index.js
+++ api/build/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-
 app.get("/", (req, res) => res.send("Welcome to the Based Meme Coins API!"));
-
 app.listen(3000, () => console.log("Server ready."));
-
 module.exports = app;
+//# sourceMappingURL=index.js.map
