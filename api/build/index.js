921b80668224ff1cb0737407db285a6ce597b0b5
Kate Aksay <gozdeaksay@gmail.com>
2024-09-05 16:42:53 -0600

    backend wip

--- api/build/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => res.send("Welcome to the Based Meme Coins API!"));
-app.listen(3000, () => console.log("Server ready."));
-module.exports = app;
-//# sourceMappingURL=index.js.map
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,6 @@
-const express = require("express");
+import express from "express";
+import { Request, Response } from "express";
 const app = express();
-
-app.get("/api/hello", (req: any, res: any) => res.send("Welcome to the Based Meme Coins API!"));
-
+app.get("/api", (req: Request, res: Response) => res.send("Welcome to the Based Meme Coins API!"));
 app.listen(3000, () => console.log("Server ready."));
-
 module.exports = app;
