cb4e66edd1ab9c499a9d53ec798eb6c463ee45e9
Kate Aksay <gozdeaksay@gmail.com>
2024-09-05 17:24:24 -0600

    vercel testing...

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
-import { Request, Response } from "express";
 const app = express();
-app.get("/api", (req: Request, res: Response) => res.send("Welcome to the Based Meme Coins API!"));
+app.get("/api", (req: any, res: any) => res.send("Welcome to the Based Meme Coins API!"));
 app.listen(3000, () => console.log("Server ready."));
 module.exports = app;
