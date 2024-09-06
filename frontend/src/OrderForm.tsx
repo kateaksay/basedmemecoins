9a9f70ac503c7276ffce2cbe8f6ad8b280a5fb3e
Kate Aksay <gozdeaksay@gmail.com>
2024-09-05 23:00:40 -0600

    fix

--- frontend/src/OrderForm.tsx
+++ frontend/src/OrderForm.tsx
@@ -14,7 +14,7 @@ export default function OrderForm({grantPermissions, permissionsContext}: {grant
   const account = useAccount();
 
   const click = async () => {
-    axios.post(import.meta.env.PUBLIC_API_URL + "/api/invest", {
+    axios.post("/api/invest", {
       prompt: order,
       amount: budget,
     }).then((response) => {
